1d55bd7d68be60b05aa6bc58fe0e7563734e9c6e
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-02 17:23:21 +0200

    implement Tag model

---
+++ TheBlog/Models/Tag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace TheBlog.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorTag { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string Text { get; set; }
+
+        // Navigation properties -> they will get the whole object they refer to
+        public virtual Post Post { get; set; }
+        public virtual IdentityUser Author { get; set; }
+
+    }
+}
