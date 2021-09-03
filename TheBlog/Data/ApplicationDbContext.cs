f9a36cc8dab86ae746044f2a3532602348625019
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-03 15:18:16 +0200

    add BlogUser type to ApplicationDbContext class (by default, it refers to IdentityUser)
    IdentityUser was swapped for BlogUser, co DbContext class should be as well

--- TheBlog/Data/ApplicationDbContext.cs
+++ TheBlog/Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TheBlog.Models;
 
 namespace TheBlog.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<BlogUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
