895ab6c043a2e309279856843cfac38e33f6dde1
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-15 13:19:25 +0200

    customize scaffolded Edit action and View (Blog controller)

--- TheBlog/Controllers/BlogsController.cs
+++ TheBlog/Controllers/BlogsController.cs
@@ -90,7 +90,7 @@ namespace TheBlog.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogUserId,Name,Description,Created,Updated,ImageData,ContentType")] Blog blog)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Image")] Blog blog)
         {
             if (id != blog.Id)
             {
