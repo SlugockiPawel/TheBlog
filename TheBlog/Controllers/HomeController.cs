d00c96f11536bcc084b83924b780225f864173f6
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-22 15:19:04 +0200

    add About and Contact Views and Actions

--- TheBlog/Controllers/HomeController.cs
+++ TheBlog/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace TheBlog.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
