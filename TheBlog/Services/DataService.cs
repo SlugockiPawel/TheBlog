c9668017a3f796200857d2e0750fa5c15dfc14c2
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-23 14:47:15 +0200

    add wrapper class that will execute tasks that needs to be done add skeleton private methods - SeedRolesAsync() and SeedUsersAsync()

--- TheBlog/Services/DataService.cs
+++ TheBlog/Services/DataService.cs
@@ -1,4 +1,5 @@
-﻿using TheBlog.Data;
+﻿using System.Threading.Tasks;
+using TheBlog.Data;
 
 namespace TheBlog.Services
 {
@@ -8,12 +9,30 @@ namespace TheBlog.Services
 
         public DataService(ApplicationDbContext dbContext)
         {
-            _dbContext = dbContext;
+            _dbContext = dbContext; // constructor injection can be done only for registered service (Startup.cs)
         }
 
-        //Task 1: Seed a few Roles into the system
+        public async Task ManageDataAsync() // wrapper class
+        {
+            //Task 1: Seed a few Roles into the system
+            await SeedRolesAsync();
 
-        //Task 2: Seed a few users into the system 
+            //Task 2: Seed a few users into the system 
+            await SeedUsersAsync();
+        }
+
+        private async Task SeedUsersAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private async Task SeedRolesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+       
 
     }
 }
