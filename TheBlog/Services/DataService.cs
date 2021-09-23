0c0a9e1079762d7a6c06b3cf77fe15c6e370cc4c
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-23 18:01:46 +0200

    start implementing SeedUsersAsync()
    - seed  adminUser to the database (via _userManager, not _dbContext)

--- TheBlog/Services/DataService.cs
+++ TheBlog/Services/DataService.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using TheBlog.Data;
 using TheBlog.Enums;
+using TheBlog.Models;
 
 namespace TheBlog.Services
 {
@@ -11,14 +12,17 @@ namespace TheBlog.Services
     {
         private readonly ApplicationDbContext _dbContext;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<BlogUser> _userManager;
 
-        public DataService(ApplicationDbContext dbContext, RoleManager<IdentityRole> roleManager)
+        public DataService(ApplicationDbContext dbContext, RoleManager<IdentityRole> roleManager,
+            UserManager<BlogUser> userManager)
         {
             _dbContext = dbContext; // constructor injection can be done only for registered service (Startup.cs)
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
-        public async Task ManageDataAsync() // wrapper class
+        public async Task ManageDataAsync() // wrapper method (method that calls other methods)
         {
             //Task 1: Seed a few Roles into the system
             await SeedRolesAsync();
@@ -29,7 +33,30 @@ namespace TheBlog.Services
 
         private async Task SeedUsersAsync()
         {
-            throw new System.NotImplementedException();
+            // If Users are already in the system, do nothing
+            // if they are not, add them to the database
+
+            if (_dbContext.Users.Any())
+            {
+                return;
+            }
+
+            // Create a new instance of BlogUser
+            var adminUser = new BlogUser()
+            {
+                Email = "slugocki.pawel@gmail.com",
+                UserName = "slugocki.pawel@gmail.com",
+                FirstName = "Pawel",
+                LastName = "Slugocki",
+                PhoneNumber = "123-456-789",
+                EmailConfirmed = true,
+            };
+
+            // Use the UserManager to create a new user that is defined by adminUser
+           await _userManager.CreateAsync(adminUser, "Abc&123!");
+
+           // Add this new user to the Administrator role
+           await _userManager.AddToRoleAsync(adminUser, BlogRole.Administrator.ToString());
         }
 
         private async Task SeedRolesAsync()
@@ -46,12 +73,8 @@ namespace TheBlog.Services
             foreach (var role in Enum.GetNames(typeof(BlogRole)))
             {
                 // need to use Role manager to create roles
-                var newRole =  await _roleManager.CreateAsync(new IdentityRole(role));
+                var newRole = await _roleManager.CreateAsync(new IdentityRole(role));
             }
         }
-
-
-       
-
     }
 }
