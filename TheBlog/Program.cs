c0fd3a2102a51bcfc2e4ec3afffa5c3dc7208c97
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-23 19:00:52 +0200

    pull out registered DataService and run ManageDataAsync() during application start

--- TheBlog/Program.cs
+++ TheBlog/Program.cs
@@ -6,21 +6,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using TheBlog.Services;
 
 namespace TheBlog
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+
+            // Pull out registered DataService
+            var dataService = host.Services.CreateScope().ServiceProvider.GetRequiredService<DataService>();
+
+            await dataService.ManageDataAsync();
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
