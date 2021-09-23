652bc968b84af66419352ec731412a8c44ee031a
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-23 15:21:21 +0200

    implement SeedRolesAsync()
    - add roles using RoleManager, not directly to the database

--- TheBlog/Services/DataService.cs
+++ TheBlog/Services/DataService.cs
@@ -1,15 +1,21 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using TheBlog.Data;
+using TheBlog.Enums;
 
 namespace TheBlog.Services
 {
     public class DataService
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public DataService(ApplicationDbContext dbContext)
+        public DataService(ApplicationDbContext dbContext, RoleManager<IdentityRole> roleManager)
         {
             _dbContext = dbContext; // constructor injection can be done only for registered service (Startup.cs)
+            _roleManager = roleManager;
         }
 
         public async Task ManageDataAsync() // wrapper class
@@ -28,7 +34,20 @@ namespace TheBlog.Services
 
         private async Task SeedRolesAsync()
         {
-            throw new System.NotImplementedException();
+            // If Roles are already in the system, do nothing
+            // if they are not, add them to the database
+
+            if (_dbContext.Roles.Any())
+            {
+                return;
+            }
+
+            // Otherwise, create set of roles
+            foreach (var role in Enum.GetNames(typeof(BlogRole)))
+            {
+                // need to use Role manager to create roles
+                var newRole =  await _roleManager.CreateAsync(new IdentityRole(role));
+            }
         }
 
 
