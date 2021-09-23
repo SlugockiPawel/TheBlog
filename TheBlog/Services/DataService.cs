e1d4f26388669def5621c4823aaafe09831d801d
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-23 14:33:51 +0200

    inject database into DataService class so it can talk to the database

--- TheBlog/Services/DataService.cs
+++ TheBlog/Services/DataService.cs
@@ -1,7 +1,16 @@
-﻿namespace TheBlog.Services
+﻿using TheBlog.Data;
+
+namespace TheBlog.Services
 {
     public class DataService
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public DataService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         //Task 1: Seed a few Roles into the system
 
         //Task 2: Seed a few users into the system 
