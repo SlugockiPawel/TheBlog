e88741c808b044b8a60d844c8909650d5e55ae18
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-23 14:29:31 +0200

    add DataService class with TODO list

---
+++ TheBlog/Services/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace TheBlog.Services
+{
+    public class DataService
+    {
+        //Task 1: Seed a few Roles into the system
+
+        //Task 2: Seed a few users into the system 
+
+    }
+}
