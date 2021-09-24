3fba5e3546de1eeaf16af687145102b9bf987c5d
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-24 13:11:43 +0200

    create EmailSettings class
    (in ViewModels folder as this model will be used for specific view only)

---
+++ TheBlog/ViewModels/EmailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace TheBlog.ViewModels
+{
+    public class EmailSettings
+    {
+        //We can configure and use an SMTP server (e.g. google)
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }  
+    }
+}
