2fb78fc3385d1ab6ad714536067003a284395356
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-24 18:10:26 +0200

    implement SendContactEmailAsync()

--- TheBlog/Services/EmailService.cs
+++ TheBlog/Services/EmailService.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.Extensions.Options;
+using MimeKit;
 using TheBlog.ViewModels;
 
 namespace TheBlog.Services
@@ -13,16 +17,33 @@ namespace TheBlog.Services
             _emailSettings = emailSettings.Value;
         }
 
-        // method of .net IEmailSender
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
-        {
-            throw new System.NotImplementedException();
-        }
-
         // our custom method
         public Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
         {
             throw new System.NotImplementedException();
+        }
+
+        // method of .net IEmailSender
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
+        {
+            var email = new MimeMessage();
+
+            email.Sender = MailboxAddress.Parse(_emailSettings.Email);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder()
+            {
+                HtmlBody = htmlMessage,
+            };
+
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_emailSettings.Host, _emailSettings.Port, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_emailSettings.Email, _emailSettings.Password);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
         }
     }
 }
