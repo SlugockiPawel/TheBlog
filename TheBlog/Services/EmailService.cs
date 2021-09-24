223da7eed22ed5d77062d9d0330f6b5a7083e99a
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-24 17:09:57 +0200

    add EmailService concreate class
    IOptions<T> as EmailService is preconfigured in Startup.cs (takes values from appsettings.json)

---
+++ TheBlog/Services/EmailService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using TheBlog.ViewModels;
+
+namespace TheBlog.Services
+{
+    public class EmailService : IBlogEmailSender
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(IOptions<EmailSettings> emailSettings) //IOptions as EmailService is preconfigured 
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        // method of .net IEmailSender
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // our custom method
+        public Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
