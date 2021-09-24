15902754ac08070b6f09ae95d7b9e3acc5b7f827
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-24 18:09:51 +0200

    make IBlogEmailSender public

--- TheBlog/Services/IBlogEmailSender.cs
+++ TheBlog/Services/IBlogEmailSender.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace TheBlog.Services
 {
-    internal interface IBlogEmailSender : IEmailSender
+    public interface IBlogEmailSender : IEmailSender
     {
         Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage);
     }
