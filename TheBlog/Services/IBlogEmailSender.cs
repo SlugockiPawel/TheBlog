d3aaf92689240998674aabfadc967fc2b8d2dc35
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-24 16:52:38 +0200

    add IBlogEmailService interface

---
+++ TheBlog/Services/IBlogEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace TheBlog.Services
+{
+    internal interface IBlogEmailSender : IEmailSender
+    {
+        Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage);
+    }
+}
