96ca684cd63aa60342c589275e5219650b0cc7cb
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-25 14:15:05 +0200

    implement SendContactEmailAsync()

--- TheBlog/Services/EmailService.cs
+++ TheBlog/Services/EmailService.cs
@@ -18,17 +18,38 @@ namespace TheBlog.Services
         }
 
         // our custom method
-        public Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
+        public async Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
         {
-            throw new System.NotImplementedException();
+            var email = new MimeMessage
+            {
+                Sender = MailboxAddress.Parse(_emailSettings.Email)
+            };
+
+            email.To.Add(MailboxAddress.Parse(_emailSettings.Email));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder();            
+            builder.HtmlBody = $"<b>{name}</b> has sent you an email and can be reached at: <b>{emailFrom}</b><br/><br/>{htmlMessage}";
+
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_emailSettings.Host, _emailSettings.Port, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_emailSettings.Email, _emailSettings.Password);
+ 
+            await smtp.SendAsync(email);
+
+            await smtp.DisconnectAsync(true);
         }
 
         // method of .net IEmailSender
         public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
         {
-            var email = new MimeMessage();
+            var email = new MimeMessage
+            {
+                Sender = MailboxAddress.Parse(_emailSettings.Email)
+            };
 
-            email.Sender = MailboxAddress.Parse(_emailSettings.Email);
             email.To.Add(MailboxAddress.Parse(emailTo));
             email.Subject = subject;
 
