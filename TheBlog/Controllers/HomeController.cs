cfe120eb2eaa4363152f68b3db72f6b55288e99a
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-25 14:14:33 +0200

    rename to _emailSender

--- TheBlog/Controllers/HomeController.cs
+++ TheBlog/Controllers/HomeController.cs
@@ -15,12 +15,12 @@ namespace TheBlog.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IBlogEmailSender _blogEmailSender;
+        private readonly IBlogEmailSender _emailSender;
 
-        public HomeController(ILogger<HomeController> logger, IBlogEmailSender blogEmailSender)
+        public HomeController(ILogger<HomeController> logger, IBlogEmailSender emailSender)
         {
             _logger = logger;
-            _blogEmailSender = blogEmailSender;
+            _emailSender = emailSender;
         }
 
         public IActionResult Index()
@@ -38,10 +38,10 @@ namespace TheBlog.Controllers
         public async Task<IActionResult> Contact(ContactMe model)
         {
             // send contact me email
+            model.Message = $"{model.Message} <hr/> Phone: {model.Phone}";
+            await _emailSender.SendContactEmailAsync(model.Email, model.Name, model.Subject, model.Message);
 
-
-
-            return View(model);
+            return RedirectToAction("Index");
         }
 
         public IActionResult About()
