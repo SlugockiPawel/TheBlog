2374bcec3e530cd08e37fff853bd4328462a0049
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-25 11:59:11 +0200

    add ContactMe ViewModel

---
+++ TheBlog/ViewModels/ContactMe.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBlog.ViewModels
+{
+    public class ContactMe
+    {
+        [Required]
+        [StringLength(80, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(50, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 5)]
+        public string Email { get; set; }
+
+        [Phone] public string Phone { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 10)]
+        public string Message { get; set; }
+    }
+}
