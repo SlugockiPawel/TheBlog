e82a96eaac7fac200fdf2264a8a06a9aad8f9e0d
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-26 15:56:00 +0200

    add IImageService interface

---
+++ TheBlog/Services/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TheBlog.Services
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile file);
+        Task<byte[]> EncodeImageAsync(string fileName);
+        string DecodeImage(byte[] data, string type);
+        string ContentType(IFormFile file);
+        int Size(IFormFile file);
+    }
+}
