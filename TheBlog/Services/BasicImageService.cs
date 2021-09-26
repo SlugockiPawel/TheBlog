572e633a0001e5bb1cb1524a2ff7d34994d94dd6
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-26 15:59:02 +0200

    add BasicImageService class that implements IImageService
    implement ContentType()

---
+++ TheBlog/Services/BasicImageService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TheBlog.Services
+{
+    public class BasicImageService : IImageService
+    {
+        public Task<byte[]> EncodeImageAsync(IFormFile file)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<byte[]> EncodeImageAsync(string fileName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string DecodeImage(byte[] data, string type)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ContentType(IFormFile file)
+        {
+            return file?.ContentType;
+        }
+
+        public int Size(IFormFile file)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
