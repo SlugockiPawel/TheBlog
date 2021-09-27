f56736dbc75d736d4f71a9534fd918aca04b27e9
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-27 15:13:02 +0200

    implement UrlFriendly()

--- TheBlog/Services/BasicSlugService.cs
+++ TheBlog/Services/BasicSlugService.cs
@@ -15,7 +15,62 @@ namespace TheBlog.Services
 
         public string UrlFriendly(string title)
         {
-            throw new System.NotImplementedException();
+            if (title == null) return "";
+
+            const int maxlen = 80;
+            var len = title.Length;
+            var prevdash = false;
+
+            var sb = new StringBuilder(len);
+
+            char c;
+            for (int i = 0; i < len; i++)
+            {
+                c = title[i];
+                if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+                {
+                    sb.Append(c);
+                    prevdash = false;
+                }
+                else if (c >= 'A' && c <= 'Z')
+                {
+                    // tricky way to convert to lowercase
+                    sb.Append((char)(c | 32));
+                    prevdash = false;
+                }
+                else if (c == ' ' || c == ',' || c == '.' || c == '/' ||
+                         c == '\\' || c == '-' || c == '_' || c == '=')
+                {
+                    if (!prevdash && sb.Length > 0)
+                    {
+                        sb.Append('-');
+                        prevdash = true;
+                    }
+                }
+                else if (c == '#')
+                {
+                    if (i > 0)
+                        if (title[i - 1] == 'C' || title[i - 1] == 'F')
+                            sb.Append("-sharp");
+                }
+                else if (c == '+')
+                {
+                    sb.Append("-plus");
+                }
+                else if ((int)c >= 128)
+                {
+                    int prevlen = sb.Length;
+                    sb.Append(RemapInternationalCharToAscii(c));
+                    if (prevlen != sb.Length) prevdash = false;
+                }
+
+                if (sb.Length == maxlen) break;
+            }
+
+            if (prevdash)
+                return sb.ToString().Substring(0, sb.Length - 1);
+            else
+                return sb.ToString();
         }
 
         public bool IsUnique(string slug)
