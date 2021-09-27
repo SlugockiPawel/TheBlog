a68bd605ae2075c53b8dd87bf699370989a4f4fb
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-27 14:53:09 +0200

    add BasicSlugService.cs

---
+++ TheBlog/Services/BasicSlugService.cs
@@ -0,0 +1,15 @@
+﻿namespace TheBlog.Services
+{
+    public class BasicSlugService : ISlugService
+    {
+        public string UrlFriendly(string title)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsUnique(string slug)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
