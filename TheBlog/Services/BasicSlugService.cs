96ce926272d634f3142a68c01841144d780e426b
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-09-27 15:11:59 +0200

    add access to the database

--- TheBlog/Services/BasicSlugService.cs
+++ TheBlog/Services/BasicSlugService.cs
@@ -1,7 +1,18 @@
-﻿namespace TheBlog.Services
+﻿using System.Linq;
+using System.Text;
+using TheBlog.Data;
+
+namespace TheBlog.Services
 {
     public class BasicSlugService : ISlugService
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BasicSlugService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public string UrlFriendly(string title)
         {
             throw new System.NotImplementedException();
