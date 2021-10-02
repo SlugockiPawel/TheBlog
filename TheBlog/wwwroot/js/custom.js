01c499676f3ff306234357182586bbeb82dda612
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-10-02 15:40:39 +0200

    add custom.js with AddTag() and DeleteTag()

---
+++ TheBlog/wwwroot/js/custom.js
@@ -0,0 +1,29 @@
+﻿let index = 0;
+
+function AddTag() {
+    const tagEntry = document.getElementById("TagEntry");
+
+    const newOption = new Option(tagEntry.value, tagEntry.value);
+    document.getElementById("TagList").options[index++] = newOption;
+
+    tagEntry.value = "";
+    return true;
+}
+
+function DeleteTag() {
+    let tagCount = 1;
+
+    while (tagCount > 0) {
+        const tagList = document.getElementById("TagList");
+        const selectedIndex =tagList.selectedIndex;
+
+        if (selectedIndex >= 0) {
+            tagList.options[selectedIndex] = null;
+            --tagCount;
+        } else {
+            tagCount = 0;
+        }
+
+        index--;
+    }
+}
