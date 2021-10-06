64f7b08fd66e13f4a8282868246789824bbb51d7
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-10-06 15:10:04 +0200

    refactor Posts/Details Action to accept slug refactor Index View to pass slug, instead of Id on asp-route-

--- TheBlog/Controllers/PostsController.cs
+++ TheBlog/Controllers/PostsController.cs
@@ -38,9 +38,10 @@ namespace TheBlog.Controllers
         }
 
         // GET: Posts/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(string slug)
         {
-            if (id == null)
+            if (string.IsNullOrWhiteSpace(slug))
             {
                 return NotFound();
             }
@@ -49,7 +50,7 @@ namespace TheBlog.Controllers
                 .Include(p => p.Blog)
                 .Include(p => p.BlogUser)
                 .Include(p => p.Tags)
-                .FirstOrDefaultAsync(m => m.Id == id);
+                .FirstOrDefaultAsync(m => m.Slug == slug);
             if (post == null)
             {
                 return NotFound();
