ba555a9c75d598684aa849fd7b49512906f53ea0
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-10-16 16:20:40 +0200

    add ConnectionService
    this will get connectionstring from Heroku(if running on heroku) or from appsettings.json (if running locally)

---
+++ TheBlog/Data/ConnectionService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace TheBlog.Data
+{
+    public class ConnectionService
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("PostgresConnection");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrWhiteSpace(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            return new NpgsqlConnectionStringBuilder()
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true,
+            }.ToString();
+        }
+    }
+}
