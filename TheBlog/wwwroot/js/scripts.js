846bc2430650857071aceafee6abc09c1b229e25
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-11-15 16:09:19 +0100

    refactor custom.js and scripts.js

--- TheBlog/wwwroot/js/scripts.js
+++ TheBlog/wwwroot/js/scripts.js
@@ -5,23 +5,22 @@
 */
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
-    const headerHeight = mainNav.clientHeight;
+    const navBar = document.getElementById('navBar');
+    const headerHeight = navBar.clientHeight;
     window.addEventListener('scroll', function() {
         const currentTop = document.body.getBoundingClientRect().top * -1;
         if ( currentTop < scrollPos) {
             // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
+            if (currentTop > 0 && navBar.classList.contains('is-fixed')) {
+                navBar.classList.add('is-visible');
             } else {
-                console.log(123);
-                mainNav.classList.remove('is-visible', 'is-fixed');
+                navBar.classList.remove('is-visible', 'is-fixed');
             }
         } else {
             // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
+            navBar.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !navBar.classList.contains('is-fixed')) {
+                navBar.classList.add('is-fixed');
             }
         }
         scrollPos = currentTop;
