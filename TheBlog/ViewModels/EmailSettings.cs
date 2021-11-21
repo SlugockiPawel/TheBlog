1fac406d4e7ee7c9d5f0b536dd8aeb5c31b3b997
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-11-21 18:07:19 +0100

    comment unused property

--- TheBlog/ViewModels/EmailSettings.cs
+++ TheBlog/ViewModels/EmailSettings.cs
@@ -4,7 +4,7 @@
     {
         //We can configure and use an SMTP server (e.g. google)
         public string Email { get; set; }
-        public string DisplayName { get; set; }
+        // public string DisplayName { get; set; }
         public string Password { get; set; }
         public string Host { get; set; }
         public int Port { get; set; }  
