b6ce68cfcd0b250122a1abff381949fad7b300bb
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-03 19:18:01 +0100

    add IsActive(0 extension method to newly created HtmlUtility class
    this method will indicate which navbar link should be active

---
+++ TheBlog/Services/HtmlUtility.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using TheBlog.Models;
+
+namespace TheBlog.Services
+{
+    public static class HtmlUtility
+    {
+        public static string IsActive(this IHtmlHelper html,
+            string control,
+            string action)
+        {
+
+
+            var routeData = html.ViewContext.RouteData;
+            var razorRoute = (routeData.Values["page"]?.ToString())?[1..]?.Split('/');
+
+            if (razorRoute is not null)
+            {
+                var razorAction = razorRoute[0];
+                var razorControl = razorRoute[1];
+
+                var returnActive = string.Equals(control, razorAction) && string.Equals(action, razorControl);
+                return returnActive ? "active" : "";
+            }
+            else
+            {
+                var routeAction = (string)routeData.Values["action"];
+                var routeControl = (string)routeData.Values["controller"];
+
+                var returnActive = string.Equals(control, routeControl) && string.Equals(action, routeAction);
+                return returnActive ? "active" : "";
+            }
+        }
+    }
+}
