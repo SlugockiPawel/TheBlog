67f480eb3ff7e09b5ac125c67dcd4ef6215aaaa4
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-13 20:37:18 +0100

    implement GetUsersWithoutRoleAsync()

--- TheBlog/Areas/Identity/Pages/Account/Manage/ManageRoles.cshtml.cs
+++ TheBlog/Areas/Identity/Pages/Account/Manage/ManageRoles.cshtml.cs
@@ -68,5 +68,27 @@ namespace TheBlog.Areas.Identity.Pages.Account.Manage
                 where role.Name == blogUserRole
                 select user).ToListAsync();
         }
+
+        private async Task<List<BlogUser>> GetUsersWithoutRoleAsync()
+        {
+            var roles = Enum.GetNames(typeof(BlogRole));
+
+            return await (from user in _context.Users
+                    join userRole in _context.UserRoles on user.Id equals userRole.UserId
+                        into userUserRoleGroup
+                    from userRole in
+                        userUserRoleGroup
+                            .DefaultIfEmpty() //DefaultIfEmpty() performs LEFT JOIN so it will return all users, not only with Roles
+                    join role in _context.Roles on userRole.RoleId equals role.Id
+                        into roleGroup
+                    from role in roleGroup.DefaultIfEmpty()
+                    select new
+                    {
+                        user,
+                        role,
+                    }
+                ).Where(x => roles.All(x2 => x2 != x.role.Name)).Select(x => x.user) // get all users without any role
+                .ToListAsync();
+        }
     }
 }
