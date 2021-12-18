a1cfd9b70837a198705c6f942d4ce80379f472e3
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-18 20:28:52 +0100

    implement ManageRoles functionality

--- TheBlog/Areas/Identity/Pages/Account/Manage/ManageRoles.cshtml.cs
+++ TheBlog/Areas/Identity/Pages/Account/Manage/ManageRoles.cshtml.cs
@@ -7,27 +7,34 @@ using System.Threading.Tasks;
 using System;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Threading;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TheBlog.Data;
 using TheBlog.Enums;
 using TheBlog.Models;
+using TheBlog.Services;
 using X.PagedList;
 
 namespace TheBlog.Areas.Identity.Pages.Account.Manage
 {
+
+    [ValidateAntiForgeryToken]
     [Authorize(Roles = "Administrator")]
     public class ManageRolesModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<BlogUser> _userManager;
 
-        public ManageRolesModel(ApplicationDbContext context)
+        public ManageRolesModel(ApplicationDbContext context, UserManager<BlogUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [BindProperty] public InputModel Input { get; set; } = new();
-
         public class InputModel
         {
             public List<BlogUser> Admins { get; set; } = new();
@@ -37,39 +44,87 @@ namespace TheBlog.Areas.Identity.Pages.Account.Manage
 
         public async Task<IActionResult> OnGetAsync()
         {
-            Input.Admins = await GetUsersByRoleAsync(BlogRole.Administrator.ToString());
-            Input.Moderators = await GetUsersByRoleAsync(BlogRole.Moderator.ToString());
-            Input.NormalUsers = await GetUsersWithoutRoleAsync();
-
+            await PopulateInputModel();
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAddRoleToUserAsync(string userId, string futureRole, [CanBeNull] string command)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
+            if (command is not null)
+            {
+                futureRole = string.Equals(command, "Admin") ? BlogRole.Administrator.ToString() : BlogRole.Moderator.ToString();
+            }
+            
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user is null)
+            {
+                return RedirectToPage();
+            }
+
+            if ( await _userManager.IsInRoleAsync(user, futureRole))
+            {
+                return RedirectToPage();
+            }
+
+            await _userManager.AddToRoleAsync(user, futureRole);
+            await _userManager.UpdateAsync(user);
 
             return RedirectToPage();
         }
 
-        private async Task<List<BlogUser>> GetUsersByRoleAsync(string blogUserRole)
+        public async Task<IActionResult> OnPostRemoveRoleFromUserAsync(string userId, string deleteRole)
         {
-            if (blogUserRole is null ||
-                !Enum.IsDefined(typeof(BlogRole), blogUserRole))
+            if (!ModelState.IsValid)
             {
-                return null;
+                return RedirectToPage();
             }
 
-            return await (from user in _context.Users
-                join userRole in _context.UserRoles on user.Id equals userRole.UserId
-                join role in _context.Roles on userRole.RoleId equals role.Id
-                where role.Name == blogUserRole
-                select user).ToListAsync();
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user is null)
+            {
+                return RedirectToPage();
+            }
+
+            if (user.Email == "slugocki.pawel@gmail.com" && deleteRole == BlogRole.Administrator.ToString())
+            {
+                return RedirectToPage();
+            }
+
+            if (!await _userManager.IsInRoleAsync(user, deleteRole))
+            {
+                return RedirectToPage();
+            }
+
+            await _userManager.RemoveFromRoleAsync(user, deleteRole);
+            await _userManager.UpdateAsync(user);
+
+            return RedirectToPage();
         }
+
+        //used framework GetUsersInRoleAsync() instead of below custom one
+
+        // private async Task<List<BlogUser>> GetUsersByRoleAsync(string blogUserRole)
+        // {
+        //     if (blogUserRole is null ||
+        //         !Enum.IsDefined(typeof(BlogRole), blogUserRole))
+        //     {
+        //         return null;
+        //     }
+        //
+        //     return await (from user in _context.Users
+        //         join userRole in _context.UserRoles on user.Id equals userRole.UserId
+        //         join role in _context.Roles on userRole.RoleId equals role.Id
+        //         where role.Name == blogUserRole
+        //         select user).ToListAsync();
+        // }
 
         private async Task<List<BlogUser>> GetUsersWithoutRoleAsync()
         {
@@ -91,6 +146,14 @@ namespace TheBlog.Areas.Identity.Pages.Account.Manage
                     }
                 ).Where(x => roles.All(x2 => x2 != x.role.Name)).Select(x => x.user) // get all users without any role
                 .ToListAsync();
+        }
+
+        private async Task PopulateInputModel()
+        {
+            Input.Admins = await _userManager.GetUsersInRoleAsync(BlogRole.Administrator.ToString()).Result.ToListAsync();
+            Input.Moderators = await _userManager.GetUsersInRoleAsync(BlogRole.Moderator.ToString()).Result.ToListAsync();
+            Input.NormalUsers = await GetUsersWithoutRoleAsync();
+
         }
     }
 }
