c7aacf6b18c6ed86b990ced7aa5514d5b2387144
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-11-07 19:06:14 +0100

    add PostsApiController to return list of PostsDTOs

---
+++ Api/PostsApiController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TheBlog.Data;
+using TheBlog.DTOs;
+using TheBlog.Enums;
+
+namespace TheBlog.Api;
+
+[Route("api/posts/")]
+[ApiController]
+public class PostsApiController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public PostsApiController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{count:int}")]
+    public ActionResult<IEnumerable<PostDto>> GetPosts(int count)
+    {
+        var posts = _context.Posts
+            .Where(p => p.ReadyStatus == ReadyStatus.ProductionReady)
+            .OrderByDescending(p => p.Created)
+            .Take(count)
+            .Select(
+                p =>
+                    new PostDto
+                    {
+                        Title = p.Title,
+                        Abstract = p.Abstract,
+                        ImageData = p.ImageData,
+                        ContentType = p.ContentType
+                    }
+            )
+            .ToList();
+
+        return Ok(posts);
+    }
+}
---
+++ DTOs/PostDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TheBlog.DTOs;
+
+public class PostDto
+{
+    public string Title { get; set; }
+    public string Abstract { get; set; }
+    public byte[] ImageData { get; set; }
+    public string ContentType { get; set; }
+}
